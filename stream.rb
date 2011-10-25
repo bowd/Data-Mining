5fa5b1b71624e3a750cc2efc3a551c240d3165cc
Bogdan Dumitru <dumitrb9@cs.man.ac.uk>
2011-10-26 00:07:26 +0100

    Mod

--- stream.rb
+++ stream.rb
@@ -6,27 +6,27 @@ require 'mongo'
 @db   = @conn['test']
 @coll = @db['tweets']
 
+#TweetStream.configure do |config|
+#  config.consumer_key = '7UAaYJecav2qpglDn788w'
+#  config.consumer_secret = 'egZW4VsfR6hvE7gppI7n1QjALaP5JQceMWJvdspUg'
+#  config.oauth_token = '46872625-f0sr2VhRsqfd8yfIywtyjG6MvRjB2vvr4NdNdE6XI'
+#  config.oauth_token_secret = 'hTNn41NFHq9mFtNvhsHpriQpixeNwoptZGLkCTWqZ8'
+#  config.auth_method = :oauth
+#  config.parser   = :yajl
+#end
+
 TweetStream.configure do |config|
-  config.consumer_key = '7UAaYJecav2qpglDn788w'
-  config.consumer_secret = 'egZW4VsfR6hvE7gppI7n1QjALaP5JQceMWJvdspUg'
-  config.oauth_token = '46872625-f0sr2VhRsqfd8yfIywtyjG6MvRjB2vvr4NdNdE6XI'
-  config.oauth_token_secret = 'hTNn41NFHq9mFtNvhsHpriQpixeNwoptZGLkCTWqZ8'
-  config.auth_method = :oauth
+  config.username = 'dumi_mrr'
+  config.password = 'jabberwocky'
+  config.auth_method = :basic
   config.parser   = :yajl
 end
-
-# TweetStream.configure do |config|
-#   config.username = 'dumi_mrr'
-#   config.password = 'jabberwocky'
-#   config.auth_method = :basic
-#   config.parser   = :yajl
-# end
 # This will pull a sample of all tweets based on
 # your Twitter account's Streaming API role.
 
-TweetStream::Client.new.firehose do |status|
-  # The status object is a special Hash with
-  # method access to its keys.
-  puts "Mama are mere"
-  @coll.insert({'text' => status.text})
+TweetStream::Client.new.sample do |status|
+  #The status object is a special Hash with
+  #method access to its keys.
+ puts "Mama are mere"
+ @coll.insert({'text' => status.text})
 end
